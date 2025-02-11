d9f89d9ab8b29365afd1a34e03c7e0de7f672e4e
Adrian <adrianbtudela@gmail.com>
2025-02-12 00:09:12 +0100

    Importación del proyecto

---
+++ sdg_app/src/services/api.js
@@ -0,0 +1,33 @@
+// services/api.js
+export const getAllCountries = async () => {
+    try {
+        const response = await fetch('https://restcountries.com/v3.1/all');
+        if (!response.ok) {
+            throw new Error('Error al obtener los datos de los países');
+        }
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error:', error);
+        return [];
+    }
+};
+
+export const getCountriesByContinent = async () => {
+    const countries = await getAllCountries();
+    const groupedByContinent = {};
+
+    countries.forEach(country => {
+        const continent = country.region || 'Unknown'; 
+        if (!groupedByContinent[continent]) {
+            groupedByContinent[continent] = [];
+        }
+        groupedByContinent[continent].push({
+            name: country.name.common, 
+            population: country.population, 
+            flag: country.flags.png, 
+        });
+    });
+
+    return groupedByContinent;
+};
